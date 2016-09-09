3b3d070db13262f6a0bcbc74e95635a2b769b8f9
Adam DePrince <adam@pelotoncycle.com>
2016-09-08 20:48:37 -0400

    Upgrade to 0.0.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='directory',
       author = 'Adam DePrince',
       author_email = 'adam@pelotoncycle.com',
       url = 'https://github.com/pelotoncycle/dir',
-      version='0.0.6',
+      version='0.0.7',
 
       description="Expose opendir/readdir/closedir",
       scripts=[
